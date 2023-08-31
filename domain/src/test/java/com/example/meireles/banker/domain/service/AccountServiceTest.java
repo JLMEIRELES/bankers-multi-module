83ad8b255899a921b21837feae77ac314c69224f
jlmeireles <joaomeireles@logusinfo.com.br>
2023-08-31 13:52:47 -0300

    Create Unit Tests for AccountService.java

---
+++ domain/src/test/java/com/example/meireles/banker/domain/service/AccountServiceTest.java
@@ -0,0 +1,65 @@
+package com.example.meireles.banker.domain.service;
+
+import com.example.meireles.banker.domain.model.Account;
+import com.example.meireles.banker.domain.model.enums.AccountTypeEnum;
+import com.example.meireles.banker.domain.provider.AccountProvider;
+import com.example.meireles.banker.domain.service.impl.AccountServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AccountServiceTest {
+
+    @InjectMocks
+    private AccountServiceImpl accountService;
+
+    @Mock
+    private AccountProvider accountProvider;
+
+    /**
+     * Checks if method {@link AccountService#addAccount(Account)} calls {@link AccountProvider} correctly
+     */
+    @Test
+    void shouldCreateAccount(){
+        //given
+        var account = spy(Account.builder().accountType(AccountTypeEnum.CURRENT).build());
+
+        when(accountProvider.addAccount(any(Account.class))).thenReturn(account);
+
+        //when
+        Account createdAccount = accountService.addAccount(account);
+
+        //then
+        Assertions.assertAll(
+                () -> Assertions.assertNotNull(createdAccount),
+                () -> Assertions.assertEquals(AccountTypeEnum.CURRENT, account.getAccountType()),
+                () -> Assertions.assertEquals(account, createdAccount),
+                () -> verify(accountProvider, times(1)).addAccount(account)
+        );
+    }
+
+    /**
+     * Checks if method {@link AccountService#addAccount(Account)} throws an exception
+     * if {@link AccountProvider#addAccount(Account)} throws an exception
+     */
+    @Test
+    void shouldNotCreateAccount(){
+        //given
+        var account = mock(Account.class);
+
+        doThrow(RuntimeException.class).when(accountProvider).addAccount(any(Account.class));
+
+        //when and then
+        Assertions.assertAll(
+                () -> Assertions.assertThrows( RuntimeException.class, () -> accountService.addAccount(account)),
+                () -> verify(accountProvider, times(1)).addAccount(account)
+        );
+    }
+}
